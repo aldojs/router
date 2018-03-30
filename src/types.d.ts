a45c8accc6fcf5c78d749a0e6463492465ac73d7
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-03-30 14:32:56 +0200

    Update src/types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,49 +1,7 @@
 
-/// <reference types="node" />
-
-import { Server } from 'http'
-
 declare type Literal = { [x: string]: any; };
 
-declare type Handler = (ctx: Context) => any;
-
-declare interface Dispatcher {
-  onError(fn: Handler): any;
-  dispatch(ctx: Context): void;
-  onFinished(fn: Handler): void;
-  register(method: string, path: string, fns: Handler[]): any;
-}
-
-declare interface Application {
-  listen(): Server;
-  get(prop: string): any;
-  finally(fn: Handler): any;
-  has(prop: string): boolean;
-  pre(...fn: Handler[]): any;
-  post(...fn: Handler[]): any;
-  catch(...fn: Handler[]): any;
-  use(...router: Router[]): any;
-  set(prop: string, value: any): this;
-  callback(): (req: Request, res: Response) => void;
-  bind(prop: string, fn: (ctx: Context) => any): this;
-  on(method: string | string[], path: string | string[], ...fns: Handler[]): this;
-}
-
-declare interface Context extends Literal {
-  error?: any;
-  req: Request;
-  res: Response;
-  params: Literal;
-}
-
-declare interface Request extends Literal {
-  url: string;
-  method: string;
-}
-
-declare interface Response extends Literal {
-  end(content?: any): void;
-}
+declare type Handler = (ctx: Literal) => any;
 
 declare interface Route {
   readonly path: string;
